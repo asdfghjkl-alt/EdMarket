d511f4e806dbc00a19a6bd28f099f39c35f16f15
Edward Wenkai Liu <liuedward180@gmail.com>
2026-01-26 21:21:09 +1100

    Removed API proxy configurations in Vercel and Vite, and used `VITE_BACKEND_URL` for direct backend API calls.

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Your backend URL
   withCredentials: true, // THIS IS MANDATORY for cookies
 });
 

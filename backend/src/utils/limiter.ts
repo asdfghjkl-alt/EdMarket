743cc1a77c34308c945d922222e1e398bc7afe2b
Edward Wenkai Liu <liuedward180@gmail.com>
2026-01-26 22:19:39 +1100

    Increased rate limit for categories and orders

--- backend/src/utils/limiter.ts
+++ backend/src/utils/limiter.ts
@@ -18,7 +18,7 @@ const authLimit = rateLimit({
 
 const orderLimit = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  limit: 25, // Limit each IP to 25 requests per `window` (here, per 1 minute).
+  limit: 30, // Limit each IP to 25 requests per `window` (here, per 1 minute).
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
@@ -26,7 +26,7 @@ const orderLimit = rateLimit({
 
 const categoryLimit = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  limit: 25, // Limit each IP to 25 requests per `window` (here, per 1 minute).
+  limit: 50, // Limit each IP to 25 requests per `window` (here, per 1 minute).
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
